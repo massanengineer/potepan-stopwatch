a1f27edfacf925ed55054ccd103a4dd26c46c6ad
massanengineer <matsuda4645@gmail.com>
2026-07-05 20:12:29 +0900

    ストップとリセット追加

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 let startTime;
+let timeoutId;
 
 document.querySelector('#start').addEventListener('click', function() {
   startTime = Date.now();
-  setInterval(function() {
+  timeoutId = setInterval(function() {
     document.querySelector('#timer').textContent = Date.now() - startTime;
   }, 10);
+});
+
+document.querySelector('#stop').addEventListener('click', function() {
+  clearInterval(timeoutId);
+});
+
+document.querySelector('#reset').addEventListener('click', function() {
+  document.querySelector('#timer').textContent = '0:0:0:0';
 });
