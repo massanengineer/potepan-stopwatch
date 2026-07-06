676c87becffabd30e86633ae175dd89d1a31e78c
massanengineer <matsuda4645@gmail.com>
2026-07-07 05:03:24 +0900

    タイマー再開機能

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 let startTime;
 let timeoutId;
+let elapsedTime = 0;
 
 document.querySelector('#start').addEventListener('click', function() {
   startTime = Date.now();
   timeoutId = setInterval(function() {
-    document.querySelector('#timer').textContent = Date.now() - startTime;
+    document.querySelector('#timer').textContent = Date.now() - startTime + elapsedTime;
   }, 10);
 });
 
 document.querySelector('#stop').addEventListener('click', function() {
   clearInterval(timeoutId);
+  elapsedTime += Date.now() - startTime;
+  document.querySelector('#timer').textContent = elapsedTime;
 });
 
 document.querySelector('#reset').addEventListener('click', function() {
+  clearInterval(timeoutId);
+  elapsedTime = 0;
   document.querySelector('#timer').textContent = '0:0:0:0';
 });
