5e1fac347cbf4a37f3e727522ca7874ea84265bb
massanengineer <matsuda4645@gmail.com>
2026-07-07 09:56:32 +0900

    ボタンの活性・非活性の制御

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let timeoutId;
 let elapsedTime = 0;
 
 document.querySelector('#start').addEventListener('click', function() {
+  setButtonRunning();
   startTime = Date.now();
   timeoutId = setInterval(function() {
     document.querySelector('#timer').textContent = Date.now() - startTime + elapsedTime;
@@ -10,13 +11,33 @@ document.querySelector('#start').addEventListener('click', function() {
 });
 
 document.querySelector('#stop').addEventListener('click', function() {
+  setButtonStopped();
   clearInterval(timeoutId);
   elapsedTime += Date.now() - startTime;
   document.querySelector('#timer').textContent = elapsedTime;
 });
 
 document.querySelector('#reset').addEventListener('click', function() {
+  setButtonStopped();
   clearInterval(timeoutId);
   elapsedTime = 0;
   document.querySelector('#timer').textContent = '0:0:0:0';
 });
+
+function setButtonInitial() {
+  document.querySelector('#start').disabled = false;
+  document.querySelector('#stop').disabled = true;
+  document.querySelector('#reset').disabled = true;
+}
+setButtonInitial();
+
+function setButtonRunning() {
+  document.querySelector('#start').disabled = true;
+  document.querySelector('#stop').disabled = false;
+  document.querySelector('#reset').disabled = true;
+}
+function setButtonStopped() {
+  document.querySelector('#start').disabled = false;
+  document.querySelector('#stop').disabled = true;
+  document.querySelector('#reset').disabled = false;
+}
